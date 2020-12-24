fa84e6de452f9ca2050a5126b3727d3e12fe6afe
EmptinessBoy <i@my.huxiaofan.com>
2020-12-24 14:27:50 +0800

    封装了bar（条形统计图）

---
+++ src/components/ChartBar.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-chart :options="bar" :auto-resize="true" />
+</template>
+
+<script>
+import ECharts from "vue-echarts"; // 在 webpack 环境下指向 components/ECharts.vue
+// 手动引入 ECharts 各模块来减小打包体积
+import "echarts/lib/chart/bar";
+import "echarts/lib/component/tooltip";
+import "echarts/lib/component/title.js";
+import "echarts/theme/fruit.js";
+export default {
+  name: "ChartBar",
+  props: ["word", "count", "name", "title", "subtext"],
+  components: {
+    "v-chart": ECharts
+  },
+  mounted() {
+    console.log(this.word);
+    console.log(this.count);
+    this.bar.yAxis.data = this.word;
+    this.bar.series[0].data = this.count;
+  },
+  data() {
+    return {
+      bar: {
+        title: {
+          text: this.title,
+          subtext: this.subtext,
+        },
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "shadow"
+          }
+        },
+        legend: {
+          data: this.name
+        },
+        grid: {
+          left: "0",
+          right: "4%",
+          bottom: "5%",
+          containLabel: true
+        },
+        xAxis: {
+          type: "value",
+          boundaryGap: [0, 0.01]
+        },
+        yAxis: {
+          type: "category",
+          data: [],
+          axisLabel: {
+            interval: 0,
+            rotate: 30
+          }
+        },
+        series: [
+          {
+            name: this.name,
+            type: "bar",
+            data: []
+          }
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.echarts.style {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/DirectorCount.vue
+++ src/views/DirectorCount.vue
@@ -1,19 +1,22 @@
 <template>
-  <v-chart :options="bar" :auto-resize="true" />
+  <e-bar
+    v-if="this.loaded === true"
+    :word="this.word"
+    :count="this.count"
+    name="TOP250作品数量"
+    title="导演作品排名"
+    subtext="入围TOP250作品少于2件的不显示"
+  ></e-bar>
+  <!--  <v-chart :options="bar" :auto-resize="true" />-->
 </template>
 
 <script>
-import ECharts from "vue-echarts"; // 在 webpack 环境下指向 components/ECharts.vue
-// 手动引入 ECharts 各模块来减小打包体积
-import "echarts/lib/chart/bar";
-import "echarts/lib/component/tooltip";
-import "echarts/lib/component/title.js";
-import "echarts/theme/fruit.js";
 import axios from "axios";
+import ChartBar from "@/components/ChartBar";
 export default {
   name: "DirectorCount",
   components: {
-    "v-chart": ECharts
+    "e-bar": ChartBar
   },
   mounted() {
     let _this = this;
@@ -21,54 +24,18 @@ export default {
       .get("/ststic/director_count.json")
       .then(response => {
         console.log(response.data);
-        _this.bar.yAxis.data = response.data.word;
-        _this.bar.series[0].data = response.data.count;
+        _this.word = response.data.word;
+        _this.count = response.data.count;
+        _this.loaded = true;
       })
       .catch()
       .finally();
   },
   data() {
     return {
-      bar: {
-        title: {
-          text: "导演作品排名",
-          subtext: "入围TOP250作品少于2件的不显示"
-        },
-        tooltip: {
-          trigger: "axis",
-          axisPointer: {
-            type: "shadow"
-          }
-        },
-        legend: {
-          data: ["TOP250作品数量"]
-        },
-        grid: {
-          left: "0",
-          right: "4%",
-          bottom: "5%",
-          containLabel: true
-        },
-        xAxis: {
-          type: "value",
-          boundaryGap: [0, 0.01]
-        },
-        yAxis: {
-          type: "category",
-          data: [],
-          axisLabel: {
-            interval: 0,
-            rotate: 30
-          },
-        },
-        series: [
-          {
-            name: "TOP250作品数量",
-            type: "bar",
-            data: []
-          }
-        ]
-      }
+      loaded: false,
+      word: [],
+      count: []
     };
   }
 };
